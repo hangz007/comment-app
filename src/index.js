64d7f263f41820493a5e3c49bf4959d0722b1af0
shenzixiang <shenzixiang@xrjiot.com>
2019-01-10 16:45:53 +0800

    删除多余代码,标记v0.1版本

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
-import React,{ Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import Clock from './Clock'
 import CommentApp from './CommentApp'
 import './index.css'
 
-class Index extends Component {
-  constructor () {
-    super()
-    this.state = { isShowClock: true }
-  }
-
-  handleShowOrHide () {
-    this.setState({
-      isShowClock: !this.state.isShowClock
-    })
-  }
-
-  render () {
-    return (
-      <div>
-        {this.state.isShowClock ? <Clock /> : null }
-        <button onClick={this.handleShowOrHide.bind(this)}>
-          显示或隐藏时钟
-        </button>
-      </div>
-    )
-  }
-}
-   
 ReactDOM.render(
         <CommentApp />,
         document.getElementById('root')
-    )
+)
